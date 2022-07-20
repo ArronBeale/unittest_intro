23cf1a7674092cc97fc1294b8f22274280dfdab9
Arron <beale86@msn.com>
2022-07-20 19:19:50 +0000

    Add even_number_of_evens function to evens.py

--- test_evens.py
+++ test_evens.py
@@ -1,7 +1,12 @@
 import unittest
-
-class TestEvebs(unittest.TestCase):
-    pass
+from evens import even_number_of_evens
 
 
-unittest.main()
+class TestEvens(unittest.TestCase):
+
+    def test_function_returns_True(self):
+        self.assertTrue(even_number_of_evens([]))
+
+
+if __name__ == '__main__':
+    unittest.main()
