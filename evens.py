33bc74328c01062ea5e0a4925dcef6ffafcae983
Arron <beale86@msn.com>
2022-07-20 19:39:36 +0000

    Add test to check if value passed is not a list, add test to check if list is empty

--- evens.py
+++ evens.py
@@ -8,7 +8,12 @@ def even_number_of_evens(numbers):
     if the number of even numbers is 7, return False
     """
 
-    return True
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+    else:
+        raise TypeError("A list was not passed into the function")
+
 
 if __name__ == '__main__':
     print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -4,8 +4,13 @@ from evens import even_number_of_evens
 
 class TestEvens(unittest.TestCase):
 
-    def test_function_returns_True(self):
-        self.assertTrue(even_number_of_evens([]))
+
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+
+
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
 
 
 if __name__ == '__main__':
