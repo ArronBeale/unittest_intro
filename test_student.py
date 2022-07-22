1872efe43f34d4305fd56875f76e1010d2734a01
Arron <beale86@msn.com>
2022-07-22 17:38:41 +0000

    Add blank lines to conform to pep8

--- test_student.py
+++ test_student.py
@@ -1,12 +1,14 @@
 import unittest
 from student import Student
 
+
 class TestStudent(unittest.TestCase):
 
     def test_full_name(self):
         student = Student('john', 'Doe')
 
         self.assertEqual(student.full_name, 'john Doe')
+
 
 if __name__ == '__main__':
     unittest.main()
