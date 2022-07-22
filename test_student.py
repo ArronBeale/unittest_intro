2663e52fe148cab586f1cb5b2bc6fc04472fe54e
Arron <beale86@msn.com>
2022-07-22 18:23:36 +0000

    Add test_email to test_student.py, add email method to student.py for test to see if correct email format is returned

--- test_student.py
+++ test_student.py
@@ -9,11 +9,15 @@ class TestStudent(unittest.TestCase):
 
         self.assertEqual(student.full_name, 'john Doe')
 
-
     def test_alert_santa(self):
         student = Student('John', 'Doe')
         student.alert_santa()
         self.assertTrue(student.naughty_list)
+
+    def test_email(self):
+        student = Student('John', 'Doe')
+
+        self.assertEqual(student.email, 'john.doe@email.com')
 
 
 if __name__ == '__main__':
