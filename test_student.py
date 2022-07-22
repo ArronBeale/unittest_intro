8f474dc068a912d988edf00846679d175eb1431d
Arron <beale86@msn.com>
2022-07-22 17:37:40 +0000

    Create instance of student class in test_student.py, add test to assert full name method returns a correctly formated student name

---
+++ test_student.py
@@ -0,0 +1,12 @@
+import unittest
+from student import Student
+
+class TestStudent(unittest.TestCase):
+
+    def test_full_name(self):
+        student = Student('john', 'Doe')
+
+        self.assertEqual(student.full_name, 'john Doe')
+
+if __name__ == '__main__':
+    unittest.main()
